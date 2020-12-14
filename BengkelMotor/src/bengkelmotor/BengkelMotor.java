d48e730b9afdbca08fbf628fe8d467fb47382256
M0A <54251384+SAPIENZz@users.noreply.github.com>
2020-12-14 15:30:55 +0700

    test
    test

--- BengkelMotor/src/bengkelmotor/BengkelMotor.java
+++ BengkelMotor/src/bengkelmotor/BengkelMotor.java
@@ -15,7 +15,7 @@ public class BengkelMotor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        test test test
     }
     
 }
