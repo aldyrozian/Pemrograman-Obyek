8282513e5d17d45323b316210680fe548a435763
ouka7788 <64126065+ouka7788@users.noreply.github.com>
2020-12-15 19:54:16 +0700

    Add files via upload

---
+++ Bengkel/build/classes/BengkelMotor/BankATM.rs
@@ -0,0 +1,5 @@
+BankATM$3
+BankATM$2
+BankATM$4
+BankATM$1
+BankATM
---
+++ Bengkel/src/bengkel/Koneksi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bengkel;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Koneksi {
+    public Connection bukaKoneksi(){
+        Connection con = null;
+        return null;
+        
+    }
+}
