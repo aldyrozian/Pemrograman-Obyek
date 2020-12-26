4c8feb64b076389ba93d1d9732762357ee886715
aldyrozian <58104637+aldyrozian@users.noreply.github.com>
2020-12-26 23:19:22 +0700

    Add files via upload

--- dbworkshop.sql
+++ dbworkshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2020 at 02:15 PM
+-- Generation Time: Dec 26, 2020 at 05:11 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -28,21 +28,56 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `isi` (
-  `no_service` int(10) NOT NULL,
-  `sparepart` varchar(20) NOT NULL,
-  `harga` varchar(10) NOT NULL,
-  `jumlah` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `no_services` varchar(20) NOT NULL,
+  `nm_sp` varchar(20) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `discount` int(20) NOT NULL,
+  `jml_bayar` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sparepart`
+-- Table structure for table `rncsparepart`
 --
 
-CREATE TABLE `sparepart` (
-  `nmsparepart` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `rncsparepart` (
+  `rincian` varchar(100) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `nm_sp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rncsparepart`
+--
+
+INSERT INTO `rncsparepart` (`rincian`, `jumlah`, `nm_sp`) VALUES
+('1.Pengecekan Bulanan\r\n2.ganti Oli', 2, '1 Bulan'),
+('1.pengecekan bulanan\r\n2.Ganti rem depan belakang\r\n3.ganti oli', 3, '2 Bulan'),
+('pengecekan bulanan', 4, '5 bulan'),
+('1.pengecekan full\r\n2.pembersihan injeksi\r\n3.ganti oli\r\n4.ganti aki\r\n5.ganti baut', 5, '10 bulan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spareparts`
+--
+
+CREATE TABLE `spareparts` (
+  `kd_sp` varchar(20) NOT NULL,
+  `nm_sp` varchar(20) NOT NULL,
+  `harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `spareparts`
+--
+
+INSERT INTO `spareparts` (`kd_sp`, `nm_sp`, `harga`) VALUES
+('SP01', '1 Bulan', 100000),
+('SP05', '10 bulan', 700000),
+('SP02', '2 Bulan', 200000),
+('SP03', '5 bulan', 500000);
 
 --
 -- Indexes for dumped tables
@@ -52,17 +87,32 @@ CREATE TABLE `sparepart` (
 -- Indexes for table `isi`
 --
 ALTER TABLE `isi`
-  ADD PRIMARY KEY (`no_service`);
+  ADD PRIMARY KEY (`no_services`),
+  ADD KEY `nm_sp` (`nm_sp`),
+  ADD KEY `jumlah` (`jumlah`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `rncsparepart`
+--
+ALTER TABLE `rncsparepart`
+  ADD PRIMARY KEY (`jumlah`),
+  ADD KEY `nm_sp` (`nm_sp`);
+
+--
+-- Indexes for table `spareparts`
+--
+ALTER TABLE `spareparts`
+  ADD PRIMARY KEY (`nm_sp`);
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `isi`
+-- Constraints for table `rncsparepart`
 --
-ALTER TABLE `isi`
-  MODIFY `no_service` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `rncsparepart`
+  ADD CONSTRAINT `rncsparepart_ibfk_1` FOREIGN KEY (`nm_sp`) REFERENCES `spareparts` (`nm_sp`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
